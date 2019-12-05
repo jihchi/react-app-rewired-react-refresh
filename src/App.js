c8f20404c3d5d8ff1d6b7827c213ca06a30e6b96
jihchi <achi@987.tw>
2019-12-05 04:47:57 +0000

    Add a counter in state for demo purpose

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setCounter(counter => counter + 1),
+      1000,
+    );
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {counter}
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
